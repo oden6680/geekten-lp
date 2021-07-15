290fee2e350b89f8e4870bd5debb56a98fcc7bcd
oden6680 <oden6680@gmail.com>
2021-07-15 16:12:15 +0900

    add header

--- lp/src/App.js
+++ lp/src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.min.css';
+import Phopne from './images/Google Pixel 4 XL Clearly White.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="hero is-primary is-large">
+      <div class="hero-head">
+        <nav class="navbar">
+          <div class="container">
+            <div class="navbar-brand">
+              <p className="title">アプリ名</p>
+            </div>
+            <div class="navbar-menu">
+              <div class="navbar-end">
+                <a class="navbar-item">
+                  ◯◯とは
+                </a>
+                <a class="navbar-item">
+                  特徴
+                </a>
+                <a class="navbar-item">
+                  利用方法
+                </a>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+
+      <div class="hero-body">
+        <div className="columns">
+          <div className="column is-half">
+            <div className="has-text-centered">
+              <img src={Phopne} alt ="Phone picture" width="40%" height="40%"></img>
+            </div>
+          </div>
+          <div className="column is-half">
+            <div className="has-text-centered">
+              <p className="title">アプリ名</p>
+              <p className="subtitle">アプリのキャッチコピー</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
